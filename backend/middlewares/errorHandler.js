4f288acc131adc77d1a542febc35e60ead9414fd
valvdov <valvdov@gmail.com>
2022-12-25 11:37:40 +0000

    hotfixes: after third review

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,10 +1,8 @@
-const router = require('express').Router();
-
-module.router.use((err, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
   const errorMessage = statusCode === 500 ? 'На сервере произошла ошибка' : message;
   res.status(statusCode).send({ message: errorMessage });
   next();
-});
+};
 
-module.exports = router;
+module.exports = errorHandler;
